8e90fc096740a6e2558996e7acbf4b87fc3043c1
Rômulo Martins Teixeira <61746313+romulomteixeira@users.noreply.github.com>
2023-01-06 20:51:22 -0300

    change port 3000 to 80;

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var app = require('./config/express')();
 var http = require('http').Server(app);
 
 
-var porta = process.env.PORT || 3000;
+var porta = process.env.PORT || 80;
 var server = http.listen(porta, function () {
 
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Running in http:/localhost:3000');
+    console.log('Running in http:/localhost:80');
 
 });
